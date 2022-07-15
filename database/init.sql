55a658aa9a64c75b7530ba2fd91586301ba7b38d
Alex Grafen <alex.grafen@gmail.com>
2022-07-15 11:19:44 +0100

    create schema

---
+++ database/init.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+DROP TABLE IF EXISTS locations CASCADE;
+
+CREATE TABLE locations (
+    code TEXT NOT NULL, 
+    name TEXT NOT NULL
+    );
+
+CREATE TABLE datasets (
+    id SERIAL PRIMARY KEY, 
+    indicator TEXT NOT NULL,
+    data JSON  
+);
+
+CREATE TABLE topics (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE datasets_topics (
+    dataset_id INTEGER REFERENCES datasets.id,
+    topic_id INTEGER REFERENCES topics.id
+);
+
+COMMIT;
