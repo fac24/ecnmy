1acdb7440a617a3062a6d1ff50121b5136211179
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-07-18 15:22:25 +0100

    extended tailwind colors with ecnm colors
    Co-authored-by: Bereket Mebrahtu <Bereketmebrahtu@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //Primary Colors
+
+        "ecnmy-skyblue": "#19C2FA",
+        "ecnmy-mint": "Hex #43D5A6",
+        "ecnmy-black": "Hex #1D1D1B",
+
+        //Primary Colors
+
+        //background color
+        "ecnmy-charcall": "#142A37",
+        "ecnmy-breeze": "#EEF0F1",
+        "ecnmy-white2": "#FFFFFF",
+        //background color
+
+        //secondary highlight colors
+        "ecnmy-cranberry": "#EA4D4E",
+        "ecnmy-tawny": "#9E6214",
+        "ecnmy-slate": "Hex #A0AFB5",
+        "ecnmy-mustard": "#DCBB06",
+        "ecnmy-grape": "#6F50FF",
+        "ecnmy-rose": "Hex #ED7A8A",
+        "ecnmy-navy": "Hex #036384",
+        "ecnmy-pumpkin": "Hex #E57333",
+        //secondary highlight colors
+      },
+    },
   },
   plugins: [],
 };
