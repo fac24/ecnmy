c9fa6df738a90cccb4b16414a374e14121565d79
Alex Grafen <alex.grafen@gmail.com>
2022-07-18 17:26:24 +0100

    start writing cards page

---
+++ pages/cards.js
@@ -0,0 +1,41 @@
+import { data } from "autoprefixer";
+
+export async function getServerSideProps(context) {
+    const query = context.query;
+
+    return {
+        props: {
+            query
+        }, // will be passed to the page component as props
+    }
+}
+
+export default function Cards({ query }) {
+    //somewhere here we get an object that we'll map
+    return (
+        <>
+            <main>
+                {/* {data.map((item) => {
+    let counter = 
+    return (<div key={}> */}
+                <div>
+                    <h2>indicator name</h2>
+                    <p>dummy text</p>
+                </div>
+                {/* )
+})} */}
+            </main>
+        </>
+    );
+}
+
+
+  //1. see what user has entered on landing page
+  //2. present dropdown menus appropriate to those (i.e. first shows borough; second topic)
+  //3. send db request to datasets table,
+  //bringing up info from those JSON objects that match the chose topic
+  //i.e. for each entry in datasets table, give name of indicator
+  //then go to the JSON for that entry, find the borough that the user has searched
+  //and present the most recent value for that indicator
+  //(further: present that indicator as a percentage of London, UK)
+  //4. present data from those topics, with a card per indicator
