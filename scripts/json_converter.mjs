407fe05e1b96ee45b6beb58c6c121aa3f1e27bc1
glombort <george.lambert99@gmail.com>
2022-07-18 10:49:29 +0100

    Added first Dataset
    File converts json to sql form:))
    
    Co-Authored-By: Alex <68148169+lascellesabercrombie@users.noreply.github.com>
    Co-Authored-By: Bereket Mebrahtu <47576080+Bereketmebrahtu@users.noreply.github.com>
    Co-Authored-By: Hussain Jawad <45372216+HusJAW@users.noreply.github.com>

--- scripts/json_converter.mjs
+++ scripts/json_converter.mjs
@@ -1,16 +1,32 @@
 import fs from "fs";
 import fetch from "node-fetch";
 
-const jsonConverter = async () => {
-
-    async function jsonParser(file) {
-        const data = JSON.parse(await fs.readFile(file));
-        return data;
-    };
-
-    const happiness = jsonParser('../datasets/happiness.json')
-
-    let happinessArray = [];
-
-
+async function jsonParser(file) {
+  /* const data = await JSON.parse(
+    fs.readFile(file, function (err, data) {
+      if (err) return console.error(err);
+      console.log(data.toString());
+    })
+  ); */
+  const rawdata = fs.readFileSync(file);
+  const data = await JSON.parse(rawdata);
+  return data;
 }
+
+const jsonConverter = async () => {
+  const happiness = await jsonParser("./datasets/happiness.json");
+
+  let happinessArray = [];
+
+  let sqlOutput = /*SQL*/ `BEGIN;\n\nINSERT INTO datasets (indicator, data) VALUES\n('happiness', '${JSON.stringify(
+    happiness
+  )}');\n\nCOMMIT;`;
+  fs.writeFile("./database/datasets.sql", sqlOutput, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log("Successfully wrote datasets.sql");
+  });
+};
+
+jsonConverter();
