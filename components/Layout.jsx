fa79cca1fd93175f3374d39854ac3dae44ee919d
glombort <george.lambert99@gmail.com>
2022-07-19 12:58:14 +0100

    Added some comments

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,21 +2,18 @@ import Head from "next/head";
 import NavBar from "./NavBar";
 
 export default function Layout({ children }) {
-    return (
-        <>
-            <Head>
-                <title>London Cost of Living Data Dashboard</title>
-                <meta
-                    name="description"
-                    content="a website to help journalists access economic data related to the cost of living crisis in London"
-                />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            </Head>
-            <NavBar />
-            <main>{children}</main>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>London Cost of Living Data Dashboard</title>
+        <meta
+          name="description"
+          content="a website to help journalists access economic data related to the cost of living crisis in London"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <NavBar />
+      <main>{children}</main>
+    </>
+  );
 }
--- pages/[location]/[topic].js
+++ pages/[location]/[topic].js
@@ -106,3 +106,13 @@ export default function Cards({ locationDatasets }) {
     </>
   );
 }
+
+//1. see what user has entered on landing page
+//2. present dropdown menus appropriate to those (i.e. first shows borough; second topic)
+//3. send db request to datasets table,
+//bringing up info from those JSON objects that match the chosen topic
+//i.e. for each entry in datasets table, give name of indicator
+//then go to the JSON for that entry, find the borough that the user has searched
+//and present the most recent value for that indicator
+//(further: present that indicator as a percentage of London, UK, ranking)
+//4. present data from those topics, with a card per indicator
