aefd0b92b13ee4bb5ea475ca718ffaccbf5a0f93
Alex Grafen <alex.grafen@gmail.com>
2022-07-19 17:05:39 +0100

    add hover definitions for jargon words like happiness

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,19 @@
+import { dictionary } from "../database/dictionary";
+import { useState } from "react";
+
+
 export default function Card({ dataset }) {
+  const [hover, setHover] = useState(false);
+
+  function handleHover() {
+    setHover(!hover);
+  }
+
   const cardData = dataset.cardData;
   return (
     <div className="border-2">
-      <h2 className="capitalize">{dataset.indicator}</h2>
       <h3>{dataset.cardData.locationData.Value}</h3>
+      {<h2 className="capitalize">{dictionary.hasOwnProperty(dataset.indicator) ? (<><span tabIndex={0} onMouseEnter={handleHover} onMouseLeave={handleHover} onFocus={handleHover} onBlur={handleHover}>{dataset.indicator}</span>{" "}<span className={`${hover ? "visible" : "invisible"}`}>{dictionary[dataset.indicator]}</span></>) : dataset.indicator}</h2>}
       <ul>
         <li>This ranks {cardData.ranking}/33 of the London Boroughs</li>
         <li>The London value is {cardData.londonData}</li>
