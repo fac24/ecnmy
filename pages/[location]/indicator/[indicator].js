56fdfa42e407753e730e033a8771d3e82cf77869
glombort <george.lambert99@gmail.com>
2022-07-20 12:55:39 +0100

    Added some metadata
    Co-Authored-By: Bereket Mebrahtu <47576080+Bereketmebrahtu@users.noreply.github.com>

--- pages/[location]/indicator/[indicator].js
+++ pages/[location]/indicator/[indicator].js
@@ -1,13 +1,44 @@
-export async function getServerSideProps() {
-    return {
-        props: {}
-    }
+import { selectDatasetByIndicator } from "../../../database/model";
+import cardDataArranger from "../../../utils/cardDataArranger";
+
+export async function getServerSideProps({ params }) {
+  const indicator = params.indicator;
+  const location = params.location;
+
+  const dataset = await selectDatasetByIndicator(indicator);
+
+  const apiURL = dataset?.metadata?.api || null;
+
+  const metadata =
+    apiURL !== null
+      ? await fetch(apiURL).then((resolve) => resolve.json())
+      : dataset?.metadata || null;
+
+  const boroughData = dataset.data.data.filter(
+    (object) => object.Geography === location
+  );
+
+  const locationDataset = cardDataArranger([dataset], location);
+
+  return {
+    props: { indicator, location, boroughData, metadata, locationDataset },
+  };
 }
 
-export default function Indicator() {
-    return (
-        <main>
-            <h1 className="blue">Indicator Page</h1>
-        </main>
-    );
+export default function Indicator({
+  indicator,
+  location,
+  boroughData,
+  metadata,
+  locationDataset,
+}) {
+  return (
+    <main>
+      <h1 className="blue">Indicator Page</h1>
+      <h2>
+        {metadata.title}: {indicator}
+      </h2>
+      <p>{metadata.description}</p>
+    </main>
+  );
 }
