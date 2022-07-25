664d40e0d6f11d42ecb2b378bf2133093584f2e9
Alex Grafen <alex.grafen@gmail.com>
2022-07-25 15:40:12 +0100

    start page setup

---
+++ pages/map.js
@@ -0,0 +1,26 @@
+// import Choropleth from "../components/Choropleth";
+
+
+async function choroGetter() {
+    const postResponse = await fetch('https://api.datawrapper.de/v3/charts', {
+        method: 'POST',
+        headers: {
+            'Authorization': process.env.API_KEY,
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            'title': 'blah',
+            'type': 'd3-lines',
+            'lastEditStep': 3
+        })
+    })
+    const postJson = await postResponse.json();
+    console.log(postJson);
+}
+
+export default function Map() {
+    choroGetter()
+    return (
+        <><div>Map Page</div></>
+    );
+}
