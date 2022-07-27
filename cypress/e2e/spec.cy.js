f599675b356fb9f449f2a2f3f1dd4bc532685742
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-07-27 15:11:33 +0100

    some tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,69 +1,70 @@
-describe("navigation", () => {
-  // it("should navigate to home page", () => {
-  //   cy.visit("/");
-  // });
-  // it("should navigate to location/topic/all page", () => {
-  //   cy.visit("/Barnet/topic/All");
-  // });
-  // it("should navigate to location page showing the location/topic/all page", () => {
-  //   cy.visit("/Barnet");
-  // });
-  // it("should navigate to a given indicator page", () => {
-  //   cy.visit("/Barnet/indicator/anxiety");
-  // });
-  // it("should navigate to the map page", () => {
-  //   cy.visit("/map");
-  // });
-});
+// describe("navigation", () => {
+// it("should navigate to home page", () => {
+//   cy.visit("/");
+// });
+// it("should navigate to location/topic/all page", () => {
+//   cy.visit("/Barnet/topic/All");
+// });
+// it("should navigate to location page showing the location/topic/all page", () => {
+//   cy.visit("/Barnet");
+// });
+// it("should navigate to a given indicator page", () => {
+//   cy.visit("/Barnet/indicator/anxiety");
+// });
+// it("should navigate to the map page", () => {
+//   cy.visit("/map");
+// });
+// });
 
-describe("Homepage tests", () => {
-  // Footertest
-  // it("should navigate to ONS webiste ", () => {
-  //   cy.visit("/")
-  //     .get('[data-test-id="ons-logo"]')
-  //     .should("have.class", "w-44")
-  //     .click();
-  // });
-  // it("should navigate to ecnmy website", () => {
-  //   cy.visit("/")
-  //     .get('[data-test-id="ecmy-logo"]')
-  //     .should("have.attr", "href", "https://www.ecnmy.org/");
-  // });
+// describe("Homepage tests", () => {
+// Footertest
+// it("should navigate to ONS webiste ", () => {
+//   cy.visit("/")
+//     .get('[data-test-id="ons-logo"]')
+//     .should("have.class", "w-44")
+//     .click();
+// });
+// it("should navigate to ecnmy website", () => {
+//   cy.visit("/")
+//     .get('[data-test-id="ecmy-logo"]')
+//     .should("have.attr", "href", "https://www.ecnmy.org/");
+// });
 
-  // Navbar
-  it(" navgate using navBar ", () => {
-    cy.visit("/").get("nav a:nth-of-type(1)").should("have.attr", "href", "/");
-  });
+// Navbar
+// it(" navgate using navBar ", () => {
+//   cy.visit("/").get("nav a:nth-of-type(1)").should("have.attr", "href", "/");
+// });
 
-  // How for homepage
-  // Dropdowns take to topics
-});
+// How for homepage
+// Dropdowns take to topics
+// });
 
 describe("Topic page tests", () => {
-  cy.get(".css-1s2u09g-control").eq(1).click()
-    cy.get([id="react-select-3-placeholder"])
+  it("can select drop down menu topic", () => {
+    cy.get(".css-14el2xx-placeholder").eq(1).click();
+    cy.get('[id = "react-select-3-placeholder"]');
   });
-  // How for topic page
-  // Navbar
-  // Footer
-  // Can see data cards
-  // Card can click
-  // Can change topic
-  // Can change location
-  // Can change both topic and location
-  // Can hover over title
-  // Can hover over i symbol
-  // Can focus on i symbol
-  //
 });
+// How for topic page
+// Navbar
+// Footer
+// Can see data cards
+// Card can click
+// Can change topic
+// Can change location
+// Can change both topic and location
+// Can hover over title
+// Can hover over i symbol
+// Can focus on i symbol
+//
 
-describe("Indicator page tests", () => {
-  // How for indicator page
-  // Navbar
-  // Footer
-  // Can see graph
-  // Can see table
-  //
-});
+// describe("Indicator page tests", () => {
+//   // How for indicator page
+//   // Navbar
+//   // Footer
+//   // Can see graph
+//   // Can see table
+//   //
+// });
 
 //cy.get('#react-select-component-id').type('Something{enter}{enter}');
